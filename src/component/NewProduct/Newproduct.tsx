a095bbe4708c48cb6237b2fa57cd2f3ab64af1e9
Hữu Hưng Nguyễn <19521571@gm.uit.edu.vn>
2024-03-29 00:19:07 +0700

    to-newproduct(productnew)

--- src/component/NewProduct/Newproduct.tsx
+++ src/component/NewProduct/Newproduct.tsx
@@ -6,7 +6,7 @@ const Newproduct = () => {
   return (
     <div>
         <section className="latest top">
-            <div className="scontainer text-center w-4/5 m-auto">
+            <div className="scontainer text-center w-4/5 m-auto md:p-12">
                 <div className="heading">
                     <h1>Latest Popular Bike</h1>
                     <div className="lines flex1">
@@ -16,12 +16,12 @@ const Newproduct = () => {
                     </div>
                 </div>
 
-                <div className="content md:grid md:grid-rows-2 gap-7 top flex flex-col">
+                <div className="content md:grid md:grid-cols-4 gap-7 top flex flex-col">
                     <div className="box p-2 md:p-8 rounded-xl border border-gray-300 transition duration-500 hover:border-green-500 cursor-pointer bg-stone-50">
                         <div className="img relative flex justify-center">
                             <img src={airblade} alt="" />
-                            <div className="flex1 flex justify-between text-green-600 absolute top-0 w-full">
-                                <label className="w-12 h-12  rounded-full text-white bg-green-600 text-center">50%</label>
+                            <div className="flex1 flex justify-between items-center text-green-600 absolute top-0 w-full">
+                                <label className="w-12 h-12 flex items-center justify-center rounded-full text-white bg-green-600 text-center">50%</label>
                                 <FontAwesomeIcon icon={faHeart} className="text-xl"/>
                             </div>
                         </div>
@@ -30,14 +30,14 @@ const Newproduct = () => {
                             <h3 className="font-semibold text-xl">Air Blade</h3>
                             <p>(Bản tiêu chuẩn)</p>
                             <h2 className="font-normal text-lg text-green-600 mx-0 my-5"> <span>Khoảng $2000</span> </h2>
-                            <button className="text-sm">Mua Ngay</button>
+                            <button className="text-sm">Xem Chi Tiết</button>
                         </div>
                     </div>
                     <div className="box p-2 md:p-8 rounded-xl border border-gray-300 transition duration-500 hover:border-green-500 cursor-pointer bg-stone-50">
                         <div className="img relative flex justify-center">
                             <img src={airblade} alt="" />
                             <div className="flex1 flex justify-between text-green-600 absolute top-0 w-full">
-                                <label className="w-12 h-12  rounded-full text-white bg-green-600 text-center">50%</label>
+                                <label className="w-12 h-12 flex items-center justify-center rounded-full text-white bg-green-600 text-center">50%</label>
                                 <FontAwesomeIcon icon={faHeart} className="text-xl"/>
                             </div>
                         </div>
@@ -46,14 +46,14 @@ const Newproduct = () => {
                             <h3 className="font-semibold text-xl">Air Blade</h3>
                             <p>(Bản tiêu chuẩn)</p>
                             <h2 className="font-normal text-lg text-green-600 mx-0 my-5"> <span>Khoảng $2000</span> </h2>
-                            <button className="text-sm">Mua Ngay</button>
+                            <button className="text-sm">Xem Chi Tiết</button>
                         </div>
                     </div>
                     <div className="box p-2 md:p-8 rounded-xl border border-gray-300 transition duration-500 hover:border-green-500 cursor-pointer bg-stone-50">
                         <div className="img relative flex justify-center">
                             <img src={airblade} alt="" />
                             <div className="flex1 flex justify-between text-green-600 absolute top-0 w-full">
-                                <label className="w-12 h-12  rounded-full text-white bg-green-600 text-center">50%</label>
+                                <label className="w-12 h-12 flex items-center justify-center rounded-full text-white bg-green-600 text-center">50%</label>
                                 <FontAwesomeIcon icon={faHeart} className="text-xl"/>
                             </div>
                         </div>
@@ -62,14 +62,14 @@ const Newproduct = () => {
                             <h3 className="font-semibold text-xl">Air Blade</h3>
                             <p>(Bản tiêu chuẩn)</p>
                             <h2 className="font-normal text-lg text-green-600 mx-0 my-5"> <span>Khoảng $2000</span> </h2>
-                            <button className="text-sm">Mua Ngay</button>
+                            <button className="text-sm">Xem Chi Tiết</button>
                         </div>
                     </div>
                     <div className="box p-2 md:p-8 rounded-xl border border-gray-300 transition duration-500 hover:border-green-500 cursor-pointer bg-stone-50">
                         <div className="img relative flex justify-center">
                             <img src={airblade} alt="" />
                             <div className="flex1 flex justify-between text-green-600 absolute top-0 w-full">
-                                <label className="w-12 h-12  rounded-full text-white bg-green-600 text-center">50%</label>
+                                <label className="w-12 h-12 flex items-center justify-center rounded-full text-white bg-green-600 text-center">50%</label>
                                 <FontAwesomeIcon icon={faHeart} className="text-xl"/>
                             </div>
                         </div>
@@ -78,14 +78,14 @@ const Newproduct = () => {
                             <h3 className="font-semibold text-xl">Air Blade</h3>
                             <p>(Bản tiêu chuẩn)</p>
                             <h2 className="font-normal text-lg text-green-600 mx-0 my-5"> <span>Khoảng $2000</span> </h2>
-                            <button className="text-sm">Mua Ngay</button>
+                            <button className="text-sm">Xem Chi Tiết</button>
                         </div>
                     </div>
                     <div className="box p-2 md:p-8 rounded-xl border border-gray-300 transition duration-500 hover:border-green-500 cursor-pointer bg-stone-50">
                         <div className="img relative flex justify-center">
                             <img src={airblade} alt="" />
                             <div className="flex1 flex justify-between text-green-600 absolute top-0 w-full">
-                                <label className="w-12 h-12  rounded-full text-white bg-green-600 text-center">50%</label>
+                                <label className="w-12 h-12 flex items-center justify-center rounded-full text-white bg-green-600 text-center">50%</label>
                                 <FontAwesomeIcon icon={faHeart} className="text-xl"/>
                             </div>
                         </div>
@@ -94,14 +94,14 @@ const Newproduct = () => {
                             <h3 className="font-semibold text-xl">Air Blade</h3>
                             <p>(Bản tiêu chuẩn)</p>
                             <h2 className="font-normal text-lg text-green-600 mx-0 my-5"> <span>Khoảng $2000</span> </h2>
-                            <button className="text-sm">Mua Ngay</button>
+                            <button className="text-sm">Xem Chi Tiết</button>
                         </div>
                     </div>
                     <div className="box p-2 md:p-8 rounded-xl border border-gray-300 transition duration-500 hover:border-green-500 cursor-pointer bg-stone-50">
                         <div className="img relative flex justify-center">
                             <img src={airblade} alt="" />
                             <div className="flex1 flex justify-between text-green-600 absolute top-0 w-full">
-                                <label className="w-12 h-12  rounded-full text-white bg-green-600 text-center">50%</label>
+                                <label className="w-12 h-12 flex items-center justify-center rounded-full text-white bg-green-600 text-center">50%</label>
                                 <FontAwesomeIcon icon={faHeart} className="text-xl"/>
                             </div>
                         </div>
@@ -110,7 +110,7 @@ const Newproduct = () => {
                             <h3 className="font-semibold text-xl">Air Blade</h3>
                             <p>(Bản tiêu chuẩn)</p>
                             <h2 className="font-normal text-lg text-green-600 mx-0 my-5"> <span>Khoảng $2000</span> </h2>
-                            <button className="text-sm">Mua Ngay</button>
+                            <button className="text-sm">Xem Chi Tiết</button>
                         </div>
                     </div>
                 </div>
